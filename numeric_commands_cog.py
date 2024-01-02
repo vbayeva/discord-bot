4991341937d41e937e8cf41a67065ae3188a09dc
Violetta <violetta.bayeva@gmail.com>
2024-01-02 17:38:46 +0100

    Add "add" command

--- numeric_commands_cog.py
+++ numeric_commands_cog.py
@@ -10,7 +10,7 @@ class Numeric(commands.Cog):
     @commands.command(name='roll')
     async def roll(self, ctx, dice: str = "d6"):
         """Rolls a dice in dN format. For example: /roll d20. If no dice size is provided, default roll size is 6."""
-        
+
         if dice.startswith('d') and dice[1:].isnumeric():
             dice_size = int(dice[1:])
 
@@ -18,6 +18,12 @@ class Numeric(commands.Cog):
             await ctx.send(f'You rolled: {result} 🎲!')
         else: 
             await ctx.send('Please specify the dice format as d<number>. For example: /roll d20')
+
+    @commands.command(name='add')
+    async def add(self, ctx, first_number: str = None, second_number: str = None):
+        if not first_number or not second_number or not first_number.isnumeric() or not second_number.isnumeric():
+            return await ctx.send('Please specify the numbers to be added. For example: /add 5 6')
+        await ctx.send(int(first_number) + int(second_number))
 
 
 async def setup(bot):
