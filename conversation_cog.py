041434bb2141d2839aee5730b00f85de5dfcf9e9
Violetta <violetta.bayeva@gmail.com>
2024-01-03 17:26:05 +0100

    Add when_joined command. Fix error with adding numbers.

--- conversation_cog.py
+++ conversation_cog.py
@@ -8,14 +8,24 @@ class Conversation(commands.Cog):
 
     @commands.command(name='emoji')
     async def emoji(self, ctx):
-        """Sends a random emocji"""
+        """Sends a random emocji."""
         await ctx.send(utils.get_random_emoji())
     
     @commands.command(name='echo')
     async def echo(self, ctx, message='nothing'):
-        """Repeats the message providing author and chanell information"""
+        """Repeats the message providing author and chanell information."""
         ctx_message = ctx.message
-        await ctx.send(f"{ctx_message.author} has written {message} at {ctx_message.created_at} in the {ctx_message.channel} chanell")
+        await ctx.send(f"{ctx_message.author} has written {message} on {utils.get_formatted_date(ctx_message.created_at)} in the {ctx_message.channel} chanell")
+
+    @commands.command(name='when_joined')
+    async def join_date(self, ctx):
+        """Prints date when you joined the server."""
+        member = ctx.author
+        join_date = member.joined_at
+
+        formatted_date = utils.get_formatted_date(join_date)
+
+        await ctx.send(f'{member.name} joined this server on {formatted_date}')
 
 async def setup(bot):
     await bot.add_cog(Conversation(bot=bot))
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ async def get_roll(dice = 6):
     return random.randint(1, dice)
 
 def are_numbers(first_number, second_number):
-    return first_number.isnumeric() or not second_number.isnumeric()
+    return not first_number.isnumeric() or not second_number.isnumeric()
 
 def add_two_numbers(first_number, second_number):
     return int(first_number) + int(second_number)
@@ -13,3 +13,6 @@ unicode_emojis = ['😄', '😃', '😀', '😊', '😉', '🙃', '🤣', '😍'
 
 def get_random_emoji():
     return random.choice(unicode_emojis)
+
+def get_formatted_date(date):
+    return date.strftime("%A, %B, %d, %Y")
